b3af6e4b67c76752ed93f72e2508b78e60bf6636
NourShloul <shloulnour31@gmail.com>
2024-07-02 21:19:25 +0300

    OOP_tasks_2/7

---
+++ OOP_tasks/02.07.2024/02.07.2024/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02._07._2024
+{
+    class Car
+    {
+        //1.Implement a Car class which has fields to store the car's make, year, Type, Price, model, Pallet No, and color
+        public string Make { get; set; }
+        public int Year { get; set; }
+        public string Type { get; set; }
+        public string Price { get; set; }
+        public string Model { get; set; }
+        public int PalletNo { get; set; }
+        public string Color { get; set; }
+        public void DisplayInfo(string make, int year) 
+        {
+            Console.WriteLine($"The Car's Make: {make}, and car's year: {year}");
+        }
+        public void DisplayInfo(string make)
+        {
+            Console.WriteLine($"The Car's Make: {make}");
+        }
+        public virtual void Display() 
+        {
+            Console.WriteLine("Welcome Car");
+        }
+    }
+    class BMW : Car
+    {
+        public override void Display()
+        {
+            Console.WriteLine("Welcome BMW");
+        }
+    }
+        internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("-------------Before Override-------------");
+            BMW bMW = new BMW();
+            bMW.Display();
+
+
+            Console.WriteLine("-------------After Override-------------");
+            BMW bMW1 = new BMW();
+            bMW1.Display();
+            Car bMW2 = new BMW();
+            bMW2.Display();
+
+
+        }
+    }
+}
---
+++ OOP_tasks/02.07.2024_2/02.07.2024_2/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace _02._07._2024_2
+{
+    interface ISound 
+    {
+        void MakeSound();
+    }
+    abstract class Animal
+    {
+        public string Name { get; set; }
+        public Animal(string name)
+        {
+            this.Name = name;
+        }
+        public abstract void Eat();
+        public void Sleep()
+        {
+            Console.WriteLine($"{Name} is sleeping");
+        }
+    }
+    class Cat:Animal,ISound
+    {
+        public Cat(string name):base(name)
+        {
+        }
+        public override void Eat()
+        {
+            Console.WriteLine($"The {Name} is eating fish");
+        }
+        public void MakeSound()
+        {
+            Console.WriteLine("Meow!");
+        }
+    }
+    class Dog:Animal,ISound
+    {
+        public Dog(string name) : base(name)
+        {
+        }
+        public override void Eat() 
+        {
+            Console.WriteLine($"The {Name} is eating chicken");
+        }
+        public void MakeSound()
+        {
+            Console.WriteLine("Woof!");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("------------CAT------------");
+            Cat cat = new Cat("Kitty");
+            cat.Eat();
+            cat.Sleep();
+            cat.MakeSound();
+
+            Console.WriteLine("------------DOG------------");
+            Dog dog = new Dog("Doggy");
+            dog.Eat();
+            dog.Sleep();
+            dog.MakeSound();
+
+        }
+    }
+}
