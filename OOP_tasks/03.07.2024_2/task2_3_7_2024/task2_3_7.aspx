8433d9df9444aaf85b948cbf90b245f807fde7c1
NourShloul <shloulnour31@gmail.com>
2024-07-04 04:15:00 +0300

    task3_7_2

---
+++ OOP_tasks/03.07.2024_2/task2_3_7_2024/task2_3_7.aspx
@@ -0,0 +1,32 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="task2_3_7.aspx.cs" Inherits="task2_3_7_2024.task2_3_7" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+             <div >
+               Result: <asp:Label ID="Label1" runat="server" Text="Result"></asp:Label>
+            </div>
+
+             <p>
+                <asp:Label ID="Label2" runat="server" Text="num1"></asp:Label>
+                <asp:TextBox ID="TextBox1" runat="server" OnTextChanged="TextBox1_TextChanged"></asp:TextBox>
+                </p>
+        <p>
+                <asp:Label ID="Label3" runat="server" Text="num2"></asp:Label>
+                <asp:TextBox ID="TextBox2" runat="server" OnTextChanged="TextBox2_TextChanged"></asp:TextBox>
+                </p>
+        <p>
+                <asp:Button ID="Button1" runat="server" Text="+" OnClick="Button1_Click" />
+                <asp:Button ID="Button2" runat="server" Text="*" OnClick="Button2_Click" />
+                <asp:Button ID="Button3" runat="server" Text="-" OnClick="Button3_Click" />
+                </p>
+        </div>
+    </form>
+</body>
+</html>
---
+++ OOP_tasks/03.07.2024_2/task2_3_7_2024/task2_3_7.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace task2_3_7_2024
+{
+    public partial class task2_3_7 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            double num1 = Convert.ToDouble(TextBox1.Text);
+            double num2 = Convert.ToDouble(TextBox2.Text);
+            double sum = num1 * num2;
+
+            Label1.Text = sum.ToString();
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            double num1 = Convert.ToDouble(TextBox1.Text);
+            double num2 = Convert.ToDouble(TextBox2.Text);
+            double sum = num1 + num2;
+
+            Label1.Text = sum.ToString();
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            double num1 = Convert.ToDouble(TextBox1.Text);
+            double num2 = Convert.ToDouble(TextBox2.Text);
+            double sum = num1 - num2;
+
+            Label1.Text = sum.ToString();
+
+        }
+
+        protected void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
